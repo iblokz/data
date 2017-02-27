09bc40efa563ffeb443092b90f7e902f2af984f2
Alex Milanov <alex@i4web.biz>
2017-02-27 18:12:37 +0200

    created test and lint tasks, added tests from old repo, some fixes

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	obj: require('lib/obj'),
+	arr: require('lib/arr'),
+	str: require('lib/obj'),
+	fn: require('lib/fn')
+};
---
+++ test/arr.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const arr = require('../lib/arr');
+
+describe('arr', () =>
+  describe('add', () =>
+    it('Creates a new array with added item on the tail end of the old array'))
+  &&
+  describe('remove', () =>
+    it('Creates a new array with a removed item from the old array'))
+  &&
+  describe('toggle', () =>
+    it('Creates a new array with either added or removed item, depending whether it exists or not'))
+);
